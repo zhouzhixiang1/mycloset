c7ff990e91d06386b9d967490bacf4c3a59a01a5
Luca <Luca@169.254.123.86>
2018-11-22 00:04:49 +0100

    aggiunta classe Outfit, Guardaroba, Vestito

---
+++ ArmadioApp/src/Guardaroba.java
@@ -0,0 +1,26 @@
+
+public class Guardaroba {
+	
+	private Integer numeroVestiti;
+
+	public Integer getNumeroVestiti() {
+		return numeroVestiti;
+	}
+
+	public void setNumeroVestiti(Integer numeroVestiti) {
+		this.numeroVestiti = numeroVestiti;
+	}
+	
+	public void aggiungiVestito(Vestito vestito) {
+		
+	}
+	
+	public void togliVestito(Vestito vestito) {
+		
+	}
+	
+	public void svuotaGuardaroba() {
+		
+	}
+
+}
---
+++ ArmadioApp/src/Outfit.java
@@ -0,0 +1,38 @@
+
+public class Outfit {
+	
+	private Vestito testa;
+	private Vestito parteSopra;
+	private Vestito parteSotto;
+	private Vestito piedi;
+	
+	public Vestito getTesta() {
+		return testa;
+	}
+	public void setTesta(Vestito testa) {
+		this.testa = testa;
+	}
+	public Vestito getParteSopra() {
+		return parteSopra;
+	}
+	public void setParteSopra(Vestito parteSopra) {
+		this.parteSopra = parteSopra;
+	}
+	public Vestito getParteSotto() {
+		return parteSotto;
+	}
+	public void setParteSotto(Vestito parteSotto) {
+		this.parteSotto = parteSotto;
+	}
+	public Vestito getPiedi() {
+		return piedi;
+	}
+	public void setPiedi(Vestito piedi) {
+		this.piedi = piedi;
+	}
+	
+	public void valutaOutfit() {
+		
+	}
+
+}
