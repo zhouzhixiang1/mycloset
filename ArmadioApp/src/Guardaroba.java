8fea715aa9f2a0b4edf6f79fccc8e16619e3d366
itsadmin <itsadmin@192.168.0.111>
2018-11-27 15:32:03 +0100

    modifica

--- ArmadioApp/src/Guardaroba.java
+++ ArmadioApp/src/Guardaroba.java
@@ -1,8 +1,25 @@
+import java.util.*;
 
 public class Guardaroba {
 	
-	private Vestiti List<Vestito>;
+	private List<Vestito> vestiti;
     
 	private Outfit outfit;
+
+	public List<Vestito> getVestiti() {
+		return vestiti;
+	}
+
+	public void setVestiti(List<Vestito> vestiti) {
+		this.vestiti = vestiti;
+	}
+
+	public Outfit getOutfit() {
+		return outfit;
+	}
+
+	public void setOutfit(Outfit outfit) {
+		this.outfit = outfit;
+	}
 
 }
