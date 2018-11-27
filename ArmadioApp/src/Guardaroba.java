0263a3e3c79a1eb9b2b16f05cd4eaf52c7af6103
gabriele <galigetta@hptmail.it>
2018-11-27 15:15:43 +0100

    guardaroba

--- ArmadioApp/src/Guardaroba.java
+++ ArmadioApp/src/Guardaroba.java
@@ -1,26 +1,8 @@
 
 public class Guardaroba {
 	
-	private Integer numeroVestiti;
-
-	public Integer getNumeroVestiti() {
-		return numeroVestigbti;
-	}
-
-	public void setNumeroVestiti(Integer numeroVestiti) {
-		this.numeroVestiti = numeroVestiti;
-	}
-	
-	public void aggiungiVestito(Vestito vestito) {
-		
-	}
-	
-	public void togliVestito(Vestito vestito) {
-		
-	}
-	
-	public void svuotaGuardaroba() {
-		
-	}
+	private Vestiti List<Vestito>;
+    
+	private Outfit outfit;
 
 }
