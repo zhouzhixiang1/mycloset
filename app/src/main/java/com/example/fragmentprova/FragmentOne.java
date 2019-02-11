a86cdff773ba015bd0ccc8e952766d62ddc7c80b
lpettenella <luca.pettenella@gmail.com>
2019-02-11 17:58:02 +0100

    ho aggiunto dei fragment e fatto un layout

--- app/src/main/java/com/example/fragmentprova/FragmentOne.java
+++ app/src/main/java/com/example/fragmentprova/FragmentOne.java
@@ -1,18 +1,27 @@
 package com.example.fragmentprova;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.style.IconMarginSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class FragmentOne extends Fragment{
 
     ImageView imageView;
     ImageView imageView2;
+    ImageButton btnConferma;
+    ImageButton btnRifiuta;
+    ImageButton btnModifica;
+    ImageButton btnCrea;
+    ImageButton btnArmadio;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -22,6 +31,43 @@ public class FragmentOne extends Fragment{
         imageView2 = view.findViewById(R.id.imageView2);
         imageView.setImageResource(R.drawable.t_shirt_maschio);
         imageView2.setImageResource(R.drawable.pantaloni_sigaretta_tasconi);
+
+        btnConferma = view.findViewById(R.id.btnConferma);
+        btnRifiuta = view.findViewById(R.id.btnRifiuta);
+        btnModifica = view.findViewById(R.id.btnModifica);
+        btnCrea = view.findViewById(R.id.btnCrea);
+        btnArmadio = view.findViewById(R.id.btnArmadio);
+
+        btnConferma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnConferma.setBackgroundResource(R.mipmap.rifiuta);
+            }
+        });
+
+        btnModifica.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragmentModificaOutfit = new FragmentModificaOutfit();
+                getFragmentManager().beginTransaction().replace(R.id.container, fragmentModificaOutfit).commit();
+            }
+        });
+
+        btnCrea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragmentAddOutfit = new FragmentAddOutfit();
+                getFragmentManager().beginTransaction().replace(R.id.container, fragmentAddOutfit).commit();
+            }
+        });
+
+        btnArmadio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragmentArmadioOutfit = new FragmentArmadioOutfit();
+                getFragmentManager().beginTransaction().replace(R.id.container, fragmentArmadioOutfit).commit();
+            }
+        });
 
         return view;
     }
