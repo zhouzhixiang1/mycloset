96caef5bbdeadd2317bb69b5bea49cbe4983a950
lpettenella <luca.pettenella@gmail.com>
2019-02-18 00:22:44 +0100

    ...

--- app/src/main/java/com/example/fragmentprova/FragmentOne.java
+++ app/src/main/java/com/example/fragmentprova/FragmentOne.java
@@ -28,7 +28,6 @@ public class FragmentOne extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_one, container, false);
-
         final DBAdapterLogin db = new DBAdapterLogin(view.getContext());
 
         imageView = view.findViewById(R.id.imageView);
